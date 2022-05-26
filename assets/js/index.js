f59fb3c34da904b21455d13d28298c25bdc3f90e
Nat Epstein <nathaniel.epstein@gmail.com>
2022-05-26 14:48:28 -0400

    search input value is connecting

---
+++ assets/js/index.js
@@ -0,0 +1,14 @@
+//DOM variables
+let searchInputEl = document.getElementById('search-input');
+let searchButton = document.querySelector('#search-button');
+
+
+//Event listener for search button
+searchButton.addEventListener('click', () => {
+    const inputValue = searchInputEl.value
+    console.log('I am working!')
+})
+
+
+
+
